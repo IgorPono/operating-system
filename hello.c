894a8f128fc334f1d8aa4b78980bf3ea60781417
Igor <iponomarenko@luc.edu>
2023-02-09 12:51:25 -0600

    Keyboard driver added

--- hello.c
+++ hello.c
@@ -12,6 +12,57 @@ uint8_t inb (uint16_t _port) {
     return rv;
 }
 
+
+void outb (uint16_t _port, uint8_t val) {
+__asm__ __volatile__ ("outb %0, %1" : : "a" (val), "dN" (_port) );
+}
+
+
+
+unsigned char keyboard_map[128] =
+{
+   0,  27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
+ '9', '0', '-', '=', '\b',     /* Backspace */
+ '\t',                 /* Tab */
+ 'q', 'w', 'e', 'r',   /* 19 */
+ 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+   0,                  /* 29   - Control */
+ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',     /* 39 */
+'\'', '`',   0,                /* Left shift */
+'\\', 'z', 'x', 'c', 'v', 'b', 'n',                    /* 49 */
+ 'm', ',', '.', '/',   0,                              /* Right shift */
+ '*',
+   0,  /* Alt */
+ ' ',  /* Space bar */
+   0,  /* Caps lock */
+   0,  /* 59 - F1 key ... > */
+   0,   0,   0,   0,   0,   0,   0,   0,  
+   0,  /* < ... F10 */
+   0,  /* 69 - Num lock*/
+   0,  /* Scroll Lock */
+   0,  /* Home key */
+   0,  /* Up Arrow */
+   0,  /* Page Up */
+ '-',
+   0,  /* Left Arrow */
+   0,  
+   0,  /* Right Arrow */
+ '+',
+   0,  /* 79 - End key*/
+   0,  /* Down Arrow */
+   0,  /* Page Down */
+   0,  /* Insert Key */
+   0,  /* Delete Key */
+   0,   0,   0,  
+   0,  /* F11 Key */
+   0,  /* F12 Key */
+   0,  /* All other keys are undefined */
+};
+
+
+
+
+
 int xPos = 0;
 int yPos = 0;
 
@@ -61,82 +112,6 @@ void main() {
     const unsigned char color = 7; // gray text on black background
 
     esp_printf(putc, "Hello\n");
-    esp_printf(putc, "Igor");
-    esp_printf(putc, "\n");
-    esp_printf(putc, "Igor");
-    esp_printf(putc, "\n");
-   esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-esp_printf(putc, "\n");
-
-
-esp_printf(putc, "Hello World");
-esp_printf(putc, "\n");
-
-esp_printf(putc, "A");
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -146,6 +121,12 @@ esp_printf(putc, "A");
 
         if(status & 1) {
             uint8_t scancode = inb(0x60);
+	    if(keyboard_map[scancode] >= 1 && keyboard_map[scancode]!=0){
+              // esp_printf(putc, "0x%02x   %c\n", ((unsigned int)scancode) & 0xff, keyboard_map[scancode]);
+	   	    if((scancode & 0x80) == 0){ 
+	   	        esp_printf(putc, "%c", keyboard_map[scancode]);
+		    }
+		 }
         }
     }
 }
